facaa886b85b69bab2e8f3b06cb34dab0242f45f
Alex Rodriguez Cid <alexrcid@hotmail.com>
2019-02-28 10:56:40 +0100

    add basic routes and 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -8,10 +9,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Components
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './modules/main/pages/main.component';
+import { PostsComponent } from './modules/posts/pages/posts/posts.component';
+import { UsersComponent } from './modules/users/pages/users/users.component';
+import { NotFoundComponent} from './modules/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'users', component: UsersComponent }, 
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    PostsComponent,
+    UsersComponent,
+    NotFoundComponent
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
