6d3fab5382c89bdce9fefad5570479f4880f0752
Alex Rodriguez Cid <alexrcid@hotmail.com>
2019-03-02 12:34:09 +0100

    get users from api

--- src/app/modules/users/pages/users/users.component.ts
+++ src/app/modules/users/pages/users/users.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent{}
 
+export class UsersComponent implements OnInit {
 
+  users: {}[] = [];
 
+  constructor(private service: UsersService) {  }
 
+  ngOnInit(): void {
+    this.service.getUsers().subscribe((data: {}[]) => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
+}
 
