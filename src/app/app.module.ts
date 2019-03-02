5af5dcae3d9689259a24f1407dcc9be53575a375
Alex Rodriguez Cid <alexrcid@hotmail.com>
2019-03-02 12:10:01 +0100

    create users services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Components
+import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './modules/main/pages/main.component';
@@ -15,7 +15,6 @@ import { PostComponent } from './modules/posts/components/post/post.component';
 import { UsersComponent } from './modules/users/pages/users/users.component';
 import { NewUserComponent } from './modules/users/pages/new-user/new-user.component';
 import { NotFoundComponent} from './modules/not-found/not-found.component';
-import { from } from 'rxjs';
 
 
 // Refactor-> Routes on selfModule
