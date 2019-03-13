7ddf7502f8dea33a1a05a2545d87f9f07b0374b6
Alex Rodriguez Cid <alexrcid@hotmail.com>
2019-03-13 16:26:00 +0100

    fix testing errors

--- src/app/modules/posts/services/posts.service.spec.ts
+++ src/app/modules/posts/services/posts.service.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { PostsService } from './posts.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PostsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => 
+  TestBed.configureTestingModule({ imports: [ HttpClientTestingModule ]}));
 
   it('should be created', () => {
     const service: PostsService = TestBed.get(PostsService);
